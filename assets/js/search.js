1255adb94a10c8509094c9dc4b7c22abcd15c9b3
antedoro <antedoro@gmail.com>
2024-06-24 18:30:15 +0200

    arberia update

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,38 +4,38 @@ const searchDataURL = '{{ $searchData.RelPermalink }}'
 const init = () => {
   const searchBox = document.querySelector('#searchBox')
   if (searchBox === null) {
-    return
+ return
   }
 
   let index = new FlexSearch.Document({
-    tokenize: 'reverse',
-    document: {
-      field: ['title', 'body'],
-      store: ['title', 'href', 'body']
-    },
+ tokenize: 'reverse',
+ document: {
+   field: ['title', 'body'],
+   store: ['title', 'href', 'body']
+ },
   })
 
   fetch(searchDataURL)
-    .then(pages => pages.json())
-    .then(pages => {
-      for(let i = 0; i < pages.length; i++){
-        index.add(i, pages[i]);
-      }
-    })
+ .then(pages => pages.json())
+ .then(pages => {
+   for(let i = 0; i < pages.length; i++){
+  index.add(i, pages[i]);
+   }
+ })
 
   searchBox.addEventListener('keyup', function (event) {
-    let searchResultsArea = document.querySelector('#searchResults')
-    let query = event.currentTarget.value
+ let searchResultsArea = document.querySelector('#searchResults')
+ let query = event.currentTarget.value
 
-    // Only trigger a search when 2 chars. at least have been provided
-    if (query.length < 2) {
-      searchResultsArea.style.display = 'none'
-      return
-    }
+ // Only trigger a search when 2 chars. at least have been provided
+ if (query.length < 2) {
+   searchResultsArea.style.display = 'none'
+   return
+ }
 
-    // Display search results
-    renderResults(index.search(query, 10, { enrich: true }));
-    searchResultsArea.style.display = 'block'
+ // Display search results
+ renderResults(index.search(query, 10, { enrich: true }));
+ searchResultsArea.style.display = 'block'
   })
 }
 
@@ -50,44 +50,44 @@ const renderResults = (results) => {
 
   // Clear search result
   while (searchResults.firstChild)
-    searchResults.removeChild(searchResults.firstChild)
+ searchResults.removeChild(searchResults.firstChild)
 
   // Show message when results is empty
   if (!results.length) {
-    let resultPage = document.createElement('div')
-    resultPage.className = 'searchResultPage'
-    resultPage.innerHTML = 'No results found for query "' + query + '"'
-    searchResults.append(resultPage)
-    return
+ let resultPage = document.createElement('div')
+ resultPage.className = 'searchResultPage'
+ resultPage.innerHTML = 'No results found for query "' + query + '"'
+ searchResults.append(resultPage)
+ return
   }
 
   let arr = results[0].result
   if (results.length > 1) {
-    arr.concat(results[1].result)
+ arr.concat(results[1].result)
   }
   arr.filter((element, index, self) =>
-    self.findIndex(e => e.id === element.id) === index)
+ self.findIndex(e => e.id === element.id) === index)
 
   let instance = new Mark(document.querySelector('#searchResults'))
   arr.forEach((result) => {
-    let resultPage = document.createElement('div')
-    resultPage.className = 'searchResultPage'
+ let resultPage = document.createElement('div')
+ resultPage.className = 'searchResultPage'
 
-    let resultTitle = document.createElement('a')
-    resultTitle.className = 'searchResultTitle'
-    resultTitle.href = result.doc.href
-    resultTitle.innerHTML = result.doc.title
-    resultPage.append(resultTitle)
+ let resultTitle = document.createElement('a')
+ resultTitle.className = 'searchResultTitle'
+ resultTitle.href = result.doc.href
+ resultTitle.innerHTML = result.doc.title
+ resultPage.append(resultTitle)
 
-    let resultBody = document.createElement('div')
-    resultBody.className = 'searchResultBody'
-    let matchPos = result.doc.body.indexOf(query)
-    let bodyStartPos = matchPos - BODY_LENGTH / 2 > 0 ? matchPos - BODY_LENGTH / 2 : 0
-    resultBody.innerHTML = result.doc.body.substr(bodyStartPos, BODY_LENGTH)
-    resultPage.append(resultBody)
-    searchResults.append(resultPage)
+ let resultBody = document.createElement('div')
+ resultBody.className = 'searchResultBody'
+ let matchPos = result.doc.body.indexOf(query)
+ let bodyStartPos = matchPos - BODY_LENGTH / 2 > 0 ? matchPos - BODY_LENGTH / 2 : 0
+ resultBody.innerHTML = result.doc.body.substr(bodyStartPos, BODY_LENGTH)
+ resultPage.append(resultBody)
+ searchResults.append(resultPage)
 
-    instance.mark(query)
+ instance.mark(query)
   })
 }
 
